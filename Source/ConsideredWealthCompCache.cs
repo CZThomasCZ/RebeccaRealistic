f9ffd27f2a77ef45b821cbf02c073a533e2f249d
Matthew05 <CZThomasCZ@email.cz>
2025-07-26 11:38:57 +0200

    Whitespace + brackets cleanup

--- Source/ConsideredWealthCompCache.cs
+++ Source/ConsideredWealthCompCache.cs
@@ -8,27 +8,35 @@ using Verse;
 
 namespace RR
 {
-    //This stores the comp on a per-map basis so that patches don't have to constantly retrieve it.
-    public static class ConsideredWealthCompCache
+  //This stores the comp on a per-map basis so that patches don't have to constantly retrieve it.
+  public static class ConsideredWealthCompCache
+  {
+    public static Dictionary<int, MapComponent_ConsideredWealth> compCachePerMap = new Dictionary<int, MapComponent_ConsideredWealth>();
+
+    public static MapComponent_ConsideredWealth GetFor(Map map)
     {
-        public static Dictionary<int, MapComponent_ConsideredWealth> compCachePerMap = new Dictionary<int, MapComponent_ConsideredWealth>();
-
-        public static MapComponent_ConsideredWealth GetFor(Map map)
-        {
-            MapComponent_ConsideredWealth comp; //Set up var.
-            if (!compCachePerMap.ContainsKey(map.uniqueID)) //If not cached..
-                compCachePerMap.Add(map.uniqueID, comp = map.GetComponent<MapComponent_ConsideredWealth>()); //Get and cache.
-            else
-                comp = compCachePerMap[map.uniqueID]; //Retrieve from cache.
-            return comp;
-        }
-
-        public static void SetFor(Map map, MapComponent_ConsideredWealth comp)
-        {
-            if (!compCachePerMap.ContainsKey(map.uniqueID)) //If not cached..
-                compCachePerMap.Add(map.uniqueID, comp); //Cache.
-            else
-                compCachePerMap[map.uniqueID] = comp; //Reassign cache.
-        }
+      MapComponent_ConsideredWealth comp; //Set up var.
+      if (!compCachePerMap.ContainsKey(map.uniqueID)) //If not cached..
+      {
+        compCachePerMap.Add(map.uniqueID, comp = map.GetComponent<MapComponent_ConsideredWealth>()); //Get and cache.
+      }
+      else
+      {
+        comp = compCachePerMap[map.uniqueID]; //Retrieve from cache.
+      }
+      return comp;
     }
+
+    public static void SetFor(Map map, MapComponent_ConsideredWealth comp)
+    {
+      if (!compCachePerMap.ContainsKey(map.uniqueID)) //If not cached..
+      {
+        compCachePerMap.Add(map.uniqueID, comp); //Cache.
+      }
+      else
+      {
+        compCachePerMap[map.uniqueID] = comp; //Reassign cache.
+      }
+    }
+  }
 }
